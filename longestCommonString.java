9b2db49988c2337567dec47b03df15a2a515d2c4
bakuakua <bakuakua@gmail.com>
2019-01-23 22:02:46 -0500

    1/23/2019 longest common substring added comments

--- longestCommonString.java
+++ longestCommonString.java
@@ -1,3 +1,5 @@
+
+public class longestCommonString{
 /**
  * Compare two strings s1, s2. Find the longest common
  * subsequence 
@@ -5,26 +7,34 @@
  * @param: String s2
  * @return: String cmnSubstr
  */
-
-public class longestCommonString{
 	public static String getCommonSubstring(String s1, String s2){
+		//initialze common substring, and length of it
     	int maxlength = 0;
     	String maxCommon = "";
+    	//scan s1 and s2 for the same character
     	for(int i = 0; i<s1.length(); i++){
     		char tmp = s1.charAt(i);
     		for(int j = 0; j<s2.length(); j++){
     			if (s2.charAt(j)==tmp){
+    			   //initialize temporary common substring and index offset
                    String common = "";
                    int offset = 0;
+                   //scan both string from respective index where first char match happens
                    while ((i+offset)<s1.length() && (j+offset)<s2.length()){
                    	   char tmpchars1 = s1.charAt(i+offset);
                    	   char tmpchars2 = s2.charAt(j+offset);
+                   	   //check for next char if matches
                        if(tmpchars2==tmpchars1){
+                       	    //append matching char to temporary common substring
+                       	    //then increment offset
                        		common += tmpchars1;
                        		offset++;
                        }
+                       //continue scanning both strings for matching chars
                        else break;
                    }
+                   //update longest common string and length if temporary common subtring
+                   //is longer than previously stored.
                    if(common.length()>maxlength) {
                    		maxlength = common.length();
                    		maxCommon = common;
